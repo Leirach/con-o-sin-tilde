3f324f04e68fd0cc9961754f4b258d36ea064102
Trevinno <Trevinno>
2020-10-24 20:35:52 -0500

    moving images without the fragments

--- app/src/main/java/com/example/proyectofinal/ActivityJuegos.kt
+++ app/src/main/java/com/example/proyectofinal/ActivityJuegos.kt
@@ -10,11 +10,9 @@ class ActivityJuegos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_juegos)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
+
+
     }
 }
