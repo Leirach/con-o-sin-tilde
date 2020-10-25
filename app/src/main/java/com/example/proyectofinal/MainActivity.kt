374736813f5ab639a7b4dbf80435c78bcc95f179
Trevinno <Trevinno>
2020-10-24 20:28:44 -0500

    moving images commit

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -7,6 +7,8 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,22 +22,71 @@ class MainActivity : AppCompatActivity() {
     lateinit var boton_leaderboard: Button
     lateinit var boton_anima: Button
     lateinit var boton_texto: Button
-
+    lateinit var image: ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        boton_juego = findViewById<Button>(R.id.button6)
+        boton_izq = findViewById<Button>(R.id.left)
+        boton_der = findViewById<Button>(R.id.right)
+        image = findViewById(R.id.logoTema)
 
-    fun pressButtonHello(view: View) {
-        Log.d("TAG", "Hallo")
+        val arregloJuegos = arrayOf<Int>(1, 2, 3)
+        var index: Int = 0;
 
-        val intent = Intent(this, MainActivity2::class.java).apply {
-            putExtra("com.example.MainActivity2.extra.MESSAGE", number)
+
+        boton_juego.setOnClickListener {
+            Log.d("TAG", "Hallo")
+            val juego_numero = arregloJuegos[index]
+            val intent = Intent(this, ActivityJuegos::class.java).apply {
+            putExtra("com.example.MainActivity2.extra.MESSAGE", juego_numero)
+            }
+            startActivityForResult(intent, TEXT_REQUEST)
         }
-        startActivityForResult(intent, TEXT_REQUEST)
+
+        boton_izq.setOnClickListener(View.OnClickListener {view ->
+            if (index == 0) {
+                index = 2
+            } else {
+                index--
+            }
+
+
+            if (index == 0) {
+                image.setImageResource(R.drawable.sega_logo);
+            } else if (index == 1) {
+                image.setImageResource(R.drawable.ejercito_mexicano);
+            } else if (index == 2) {
+                image.setImageResource(R.drawable.watermelon);
+            }
+        })
+
+        boton_der.setOnClickListener(View.OnClickListener { view ->
+            if (index == 2) {
+                index = 0
+            } else {
+                index++
+            }
+
+            if (index == 0) {
+                image.setImageResource(R.drawable.sega_logo);
+            } else if (index == 1) {
+                image.setImageResource(R.drawable.ejercito_mexicano);
+            } else if (index == 2) {
+                image.setImageResource(R.drawable.watermelon);
+            }
+
+
+        })
+
+
+
+
     }
+
+
 
 
 
