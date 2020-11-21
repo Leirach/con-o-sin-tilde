8bdb18ed28383d2993c6b97d8652df28b4734a17
Leirach <juandleon27@gmail.com>
2020-11-21 17:04:52 -0600

    added new logo, added explanation videos

--- app/src/main/java/com/leirach/acentos/AnimationActivity.kt
+++ app/src/main/java/com/leirach/acentos/AnimationActivity.kt
@@ -1,20 +1,55 @@
 package com.leirach.acentos
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class AnimationActivity : AppCompatActivity() {
+    lateinit var videoView: VideoView
+    lateinit var btnContinue: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animation)
 
-        var number: Int = intent.getIntExtra("com.example.extra.GAME_MODE", 0)
-        val btnContinue = findViewById<Button>(R.id.continuar)
+        toggleActionBar(resources.configuration.orientation) // hide/show action bar
+
+        val gameNumber: Int = intent.getIntExtra("com.example.extra.GAME_MODE", 0)
+        btnContinue = findViewById<Button>(R.id.continuar)
+        videoView = findViewById(R.id.animation_video)
+
+
+        val path = when (gameNumber) {
+            R.id.regla_general -> {
+                "android.resource://" + packageName + "/" + R.raw.regla_general
+            }
+            R.id.contexto -> {
+                "android.resource://" + packageName + "/" + R.raw.contexto
+            }
+            R.id.hiato -> {
+                "android.resource://" + packageName + "/" + R.raw.hiatos
+            }
+            else -> {
+                "android.resource://" + packageName + "/" + R.raw.regla_general
+            }
+        }
+
+        videoView.setVideoPath(path)
+        val mediaController = MediaController(this)
+        videoView.setMediaController(mediaController)
+        videoView.start()
+
+        videoView.onfi
 
         btnContinue.setOnClickListener {
-            when (number) {
+            when (gameNumber) {
                 R.id.regla_general -> {
                     val intent = Intent(this, ActivityReglaGeneral::class.java)
                     startActivity(intent)
@@ -28,6 +63,31 @@ class AnimationActivity : AppCompatActivity() {
                     startActivity(intent)
                 }
             }
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggleActionBar(newConfig.orientation)
+    }
+
+    // toggle action bar depending on sent orientation parameter
+    fun toggleActionBar(orientation: Int) {
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            supportActionBar?.hide()
+        }
+        else {
+            supportActionBar?.show()
+        }
+    }
+
+    // show hide skip button when screen is tapped
+    fun screenTap(view: View) {
+        if (btnContinue.visibility == View.VISIBLE){
+            btnContinue.visibility = View.INVISIBLE
+        }
+        else {
+            btnContinue.visibility = View.VISIBLE
         }
     }
 }
